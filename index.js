24892db5ad9749bd283a357f8976e1aeaaf449f3
Konstantinos Arvanitakis <ksarvanitakis@hotmail.com>
2024-02-15 11:36:23 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+let Apollonia = {
+    name: "Apolonia skola",
+    adress: "Kungsgatan 54",
+    zipcode: 11856,
+    city: "Stockholm",
+    students: [],
+    teachers: [],
+};
+
+let language = {
+    name: "Math",
+    students: [],
+    teacher: []
+};
+
+let Aris = {
+    name: "Aris",
+    age: 16,
+    gender: "man",
+    subjects: []
+};
+
+let Peter = {
+    name: "Peter",
+    age: 16,
+    gender: "man",
+    subjects: []
+};
+
+let Elpida = {
+    name: "Elpida",
+    age: 16,
+    gender: "woman",
+    subjects: []
+};
+
+let Niklas = {
+    name: "Niklas",
+    subjects: []
+};
+
+let Thomas = {
+    name: "Thomas",
+    subjects: []
+};
+
+const newCourse = Thomas.subjects.push('History');
+
+console.log(newCourse)
